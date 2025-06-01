c87cf41060888aac56337e0091ac3d48a8978077
Aadithya Sai <aadithyas0610@gmail.com>
2025-06-01 21:57:57 +0530

    log non 2xx codes as warnings

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -5,7 +5,13 @@ const logger = LoggerService.getInstance("loggerMiddleware()");
 
 function loggerMiddleware(req: Request, res: Response, next: NextFunction) {
   res.on("finish", () => {
-    logger.info(`${res.statusCode} ${req.method} ${req.originalUrl}`);
+    if (200 <= res.statusCode && res.statusCode <= 299) {
+      // 2xx Success
+      logger.info(`${res.statusCode} ${req.method} ${req.originalUrl}`);
+    } else {
+      // Error codes
+      logger.warn(`${res.statusCode} ${req.method} ${req.originalUrl}`);
+    }
   });
   next();
 }
