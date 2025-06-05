ad23adab3fe4d9aef2f83fde3bc3a583e8e40d95
Aadithya Sai <aadithyas0610@gmail.com>
2025-06-05 09:23:45 +0530

    use warn for non 2xx responses

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -5,14 +5,15 @@ const logger = LoggerService.getInstance("loggerMiddleware()");
 
 function loggerMiddleware(req: Request, res: Response, next: NextFunction) {
   res.on("finish", () => {
-    if (200 <= res.statusCode && res.statusCode <= 299) {
-      // 2xx Success
+    if (200 <= res.statusCode && res.statusCode <= 399) {
+      // 2xx Success and 3xx Redirect
       logger.info(`${res.statusCode} ${req.method} ${req.originalUrl}`);
     } else {
       // Error codes
       logger.warn(`${res.statusCode} ${req.method} ${req.originalUrl}`);
     }
   });
+
   next();
 }
 
